a72ca4cbe109b488bf5d5e3e4c6be1431f89f374
ozturkkerem <ozturk.kerem@live.com>
2022-01-21 09:18:26 -0500

    set class Update

---
+++ src/Replit/Library/Exception.java
@@ -0,0 +1,36 @@
+package Replit.Library;
+
+import Day06.SoppingListReplit;
+
+import java.io.FileInputStream;
+
+public class Exception {
+
+    // check exception and unchecked exception( has is a relationship)
+
+    public static void main(String[] args) {
+
+        Integer [] arr = {10,20,30};
+
+//        System.out.println(arr[10]);
+
+
+  //      System.out.println("jAva".charAt(-1));
+
+   // String str = null;
+
+       // System.out.println(str.toUpperCase());// unchecked exception
+        System.out.println("-------------------------------------");
+
+        //Thread.sleep(3000);
+
+      //  FileInputStream file = new FileInputStream("file path");
+
+    }
+
+
+
+
+
+
+}
---
+++ src/Replit/Library/Exception_Handling.java
@@ -0,0 +1,11 @@
+package Replit.Library;
+
+public class Exception_Handling {
+
+
+
+
+
+
+
+}
